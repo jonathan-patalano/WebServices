fed1049522b128229caf260465eb47d03a4b1c8d
Jpatalano <jonathan.patalano@ynov.com>
2019-11-06 17:49:02 +0100

    Feat : Base du projet

---
+++ TodoApi/Controllers/UtilisateursController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateursController : ControllerBase
+    {
+        private readonly UtilisateurContext _context;
+
+        public UtilisateursController(UtilisateurContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Utilisateurs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Utilisateur>>> GetUtilisateurs()
+        {
+            return await _context.Utilisateurs.ToListAsync();
+        }
+
+        // GET: api/Utilisateurs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Utilisateur>> GetUtilisateur(long id)
+        {
+            var utilisateur = await _context.Utilisateurs.FindAsync(id);
+
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return utilisateur;
+        }
+
+        // PUT: api/Utilisateurs/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUtilisateur(long id, Utilisateur utilisateur)
+        {
+            if (id != utilisateur.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(utilisateur).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UtilisateurExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Utilisateurs
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Utilisateur>> PostUtilisateur(Utilisateur utilisateur)
+        {
+            _context.Utilisateurs.Add(utilisateur);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUtilisateur), new { id = utilisateur.Id }, utilisateur);
+        }
+
+        // DELETE: api/Utilisateurs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Utilisateur>> DeleteUtilisateur(long id)
+        {
+            var utilisateur = await _context.Utilisateurs.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            _context.Utilisateurs.Remove(utilisateur);
+            await _context.SaveChangesAsync();
+
+            return utilisateur;
+        }
+
+        private bool UtilisateurExists(long id)
+        {
+            return _context.Utilisateurs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TodoApi/Models/Utilisateur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Utilisateur
+    {
+        public long Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Adresse { get; set; }
+    }
+}
